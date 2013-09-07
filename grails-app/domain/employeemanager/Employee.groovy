5662154f42ddf4dddb1742b4a15fa8ef1f5b1d80
Xavierwoo88 <lalaboi88@yahoo.com>
2013-09-07 14:34:45 +0800

    Revert "input middlename"
    This reverts commit b9addd71381658d7e71ca530c919f132c68603b5.

--- grails-app/domain/employeemanager/Employee.groovy
+++ grails-app/domain/employeemanager/Employee.groovy
@@ -3,14 +3,12 @@ package employeemanager
 class Employee {
         String firstName
         String lastName
-        String middleName
         Date birthday
         String description
 
        static constraints = { 
            firstName(blank:false, unique:true) 
            lastName(blank:false)
-           middleName(blank:false, unique:true) 
            birthday(nullable:true)
            description(maxSize:1000, nullable:true)
        }   
